29040be62ee768d70baa24916e11dbd49e5e05e8
Unknown <Matthiaszb626@gmail.com>
2017-08-31 11:19:09 -0600

    Having fun

---
+++ Gusser/Assets/Guessing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guessing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Welcome to Number Guesser");
+		print ("Pick a Number in your head");
+
+		print ("The highest number you can pick is 100");
+		print ("the lowest number you can pick is 1");
+
+		print ("Is the number higher or lower than 50");
+		print ("Up arrow for higher, Down for lower, Enter for equal");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
