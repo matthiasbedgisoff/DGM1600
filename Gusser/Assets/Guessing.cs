3352b813208ea31961c61b6d7ab8dc10aa678ff8
Unknown <Matthiaszb626@gmail.com>
2017-09-07 17:23:13 -0600

    finish game

--- Gusser/Assets/Guessing.cs
+++ Gusser/Assets/Guessing.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class Guessing : MonoBehaviour {
 
 
-	int max = 100;
-	int min = 1;
-	int guess = 50;
+	private int max = 100;
+	private int min = 1;
+	private int guess;
+
+	public int counter;
 
 	// Use this for initialization
 	void Start () 
 	{
-
+		guess = Random.Range (min,max);
 		print ("Welcome to Number Guesser");
 		print ("Pick a Number in your head");
 
@@ -26,22 +28,40 @@ public class Guessing : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.UpArrow))
-		{
+
+		if (counter == -1) {
+			//counter--;
+
+
+			if (Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.DownArrow)) {
+				//counter--;
+				print ("You win! Humanity has beaten me for now");
+			}
+
+
+		}
+
+		else if (Input.GetKeyDown (KeyCode.UpArrow)) {
 			min = guess;  
 			guess = (max + min) / 2;
-			print ("Is the number higher or lower than "+guess);
+			counter--;
+			print ("Is the number higher or lower than " + guess);
 		}
-		if (Input.GetKeyDown (KeyCode.DownArrow))
-
-		{
+		else if (Input.GetKeyDown (KeyCode.DownArrow)) {
 			max = guess;	
 			guess = (max + min) / 2;
-			print ("Is the number higher or lower than "+guess);
+			counter--;
+			print ("Is the number higher or lower than " + guess);
 		}	
-		if (Input.GetKeyDown (KeyCode.Return)) 
-		{
-			print ("I win");
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			print ("I win, humanity is unintelligent");
+		
+		}
+
+
+		if (counter == 0){
+			counter--;
+		}
+
 		}
 	}
-}
