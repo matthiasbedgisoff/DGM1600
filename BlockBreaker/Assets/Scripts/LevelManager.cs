d8aef9b5730fa7b5f921235b1a4f545ec7ede541
Unknown <Matthiaszb626@gmail.com>
2017-10-06 10:43:37 -0600

    Block breaker game 01

---
+++ BlockBreaker/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelManager : MonoBehaviour {
+
+	public void Levelload (string name) {
+		SceneManager.LoadScene (name);
+	}
+
+
+	public void ExitGame () {
+		print ("Tried to Exit.");
+		Application.Quit ();
+	}
+}
