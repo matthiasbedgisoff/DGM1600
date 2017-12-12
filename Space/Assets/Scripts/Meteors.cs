fabe764c37bc34174746ba848bebeefc3c020b4e
matthiasbedgisoff <Matthiaszb626@gmail.com>
2017-12-11 22:18:39 -0700

    Update Meteors.cs
    added commit

--- Space/Assets/Scripts/Meteors.cs
+++ Space/Assets/Scripts/Meteors.cs
@@ -8,7 +8,7 @@ public class Meteors : MonoBehaviour {
 
 	public float startingSpin;
 
-
+//rotation of the meteors
 	void Start () {
 		GetComponent<Rigidbody2D>().AddTorque(Random.Range(-startingSpin,startingSpin),ForceMode2D.Impulse);
 	
