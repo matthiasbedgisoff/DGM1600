378ade9ba3cce88bd87c7b9e3743f24839f16190
matthiasbedgisoff <Matthiaszb626@gmail.com>
2017-12-12 10:00:30 -0700

    Add files via upload
    intro to unity

---
+++ intro to unity environment/Assets/scripts/health.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+private Health;
+
+public class health : MonoBehaviour {
+	
+
+	public void TakeDamage(int amount)
+	{
+		Health -= amount;
+		if (Health <= 0)
+		{
+			Health = 0;
+			Debug.Log("Dead!");
+		}
+	}
+}
